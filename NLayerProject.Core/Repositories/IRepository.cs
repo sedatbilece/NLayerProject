bbbf9a6fefc7ec494f6b44014334f69f833345d9
Sedat Bilece <seda1tbilece@gmail.com>
2021-04-25 16:16:49 +0300

    IRepository added

---
+++ NLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    interface IRepository <TEntity> where TEntity:class// we  definated parameter type  mean is class
+    {
+
+        // task for async 
+        Task< TEntity> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+
+        //  as find (x=> x.id=..) 
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+
+    }
+}
