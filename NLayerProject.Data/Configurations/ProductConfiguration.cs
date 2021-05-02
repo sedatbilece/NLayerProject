a685a1a5f406586281323f5aed8b4975224aebd4
Sedat Bilece <seda1tbilece@gmail.com>
2021-05-02 14:45:12 +0300

    added product config to data layer

---
+++ NLayerProject.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Data.Configurations
+{
+
+    // config file extending 
+    class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+
+        // interface implemeting
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+
+            builder.HasKey(x => x.Id);// id is primary key
+
+            builder.Property(x => x.Id).UseIdentityColumn();// 
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);// name required and max 200 char
+
+            builder.Property(x => x.Stock).IsRequired();
+
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");// price is like as 14552236.50 
+
+            builder.Property(x => x.InnerBarcode).HasMaxLength(50);
+
+        }
+    }
+}
