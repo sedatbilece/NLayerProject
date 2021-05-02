ce37c375c04a1cf13006db4a474fea26a2ea9264
Sedat Bilece <seda1tbilece@gmail.com>
2021-05-02 14:30:04 +0300

    added dbcontext to data layer

---
+++ NLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Data
+{
+    class AppDbContext :DbContext
+    {
+        // base options is switcher to db type  sql ,mysql or something
+        public AppDbContext(DbContextOptions<AppDbContext> options):base (options)
+        {
+                
+        }
+
+
+        // for database table 
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> prudocts { get; set; }
+
+
+        // tables options here  while creating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+    }
+}
