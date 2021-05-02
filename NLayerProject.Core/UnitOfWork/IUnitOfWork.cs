e6bb6dc5581d0a505c8ca2fa7568dc1fe598d591
Sedat Bilece <seda1tbilece@gmail.com>
2021-05-02 10:30:18 +0300

    added unitofwork

---
+++ NLayerProject.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using NLayerProject.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.UnitOfWork
+{
+    interface IUnitOfWork
+    {
+        // this about your wish
+        IProductRepository products { get; }
+
+        ICategoryRepository categories { get; }
+
+
+
+
+
+        // this is our func interface
+        Task CommitAsync();
+
+        void Commit();
+
+    }
+}
